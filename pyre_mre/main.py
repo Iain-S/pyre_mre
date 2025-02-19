132da031a7c71c5bc47a2510b89365a08cdc82f6
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-02-19 11:38:44 +0000

    Initial commit

---
+++ pyre_mre/main.py
@@ -0,0 +1,10 @@
+"""Minimal reproducible example of possible Pyre-check bug."""
+from collections.abc import Generator
+
+
+def infinite_stream(start: int) -> Generator[int]:
+    """From https://docs.python.org/3/library/typing.html#annotating-generators-and-coroutines."""
+    while True:
+        yield start
+        start += 1
+
